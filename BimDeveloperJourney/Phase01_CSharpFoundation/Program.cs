8604fcf1fb72ac1f99ff7e73b431dc8e1afc6153
fahmy56 <fahmy56@example.com>
2026-06-02 22:52:59 +0400

    session 3 — constructors and access modifiers

---
+++ BimDeveloperJourney/Phase01_CSharpFoundation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Phase01_CSharpFoundation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StructuralColumn c1 = new StructuralColumn("C1", 3000, "300x300", "C30");
+            StructuralColumn c2 = new StructuralColumn("C2", 3500, "400x300", "C35");
+            StructuralColumn c3 = new StructuralColumn("C3", 2800, "500x300", "C30");
+
+            c1.PrintInfo();
+            c2.PrintInfo();
+            c3.PrintInfo();
+
+            try
+            {
+                StructuralColumn c4 = new StructuralColumn("C4", -500, "300x300", "C30");
+                c4.PrintInfo();
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"Cannot create column: {ex.ParamName}");
+            }
+        }
+    }
+}
---
+++ BimDeveloperJourney/Phase01_CSharpFoundation/StructuralColumn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phase01_CSharpFoundation
+{
+    public class StructuralColumn
+    {
+        private string _mark;
+        private double _height;
+        private string _section;
+        private string _concreteGrade;
+
+        public string Mark
+        {
+            get {  return _mark; }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+        }
+
+        public string Section
+        {
+            get { return _section; }
+        }
+
+        public string ConcreteGrade
+        {
+            get { return _concreteGrade; }
+        }
+
+        public StructuralColumn(string mark, double height, string section, string concreteGrade)
+        {
+            if (string.IsNullOrWhiteSpace(mark))
+                throw new ArgumentNullException("Mark cannot be empty");
+
+            if (height <= 0)
+                throw new ArgumentOutOfRangeException("Height must be greater than zero");
+
+            _mark = mark;
+            _height = height; 
+            _section = section;
+            _concreteGrade = concreteGrade;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Mark          : {Mark}");
+            Console.WriteLine($"Height        : {Height}mm");
+            Console.WriteLine($"Section       : {Section}");
+            Console.WriteLine($"Concrete Grade: {ConcreteGrade}");
+        }
+    }
+}
